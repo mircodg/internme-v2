249784c9d576f692e62171181e385b4b962d3ba0
Domenico Germanà <Mirkogermana12@gmail.com>
2025-07-06 13:05:14 +0200

    feat: integrate Clerk for authentication and update dependencies

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// CONVEX-CLERK AUTH CONFIG
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      applicationID: "convex",
+    },
+  ],
+};
